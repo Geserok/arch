f1277fbb0d5b951e702c557954ccefed42c36e70
geserok <geserok@gmail.com>
2018-07-18 10:07:09 +0300

    Working commit 18.07.18

---
+++ src/main/java/repository/SupplyModule.java
@@ -0,0 +1,39 @@
+package repository;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class SupplyModule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "SupplyModule_id")
+    int id;
+
+    @Column(name = "SupplyModuleName")
+    String name;
+
+    @Column(name = "decimalNumber", unique = true, nullable = false)
+    String decimalNumber;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDecimalNumber(String decimalNumber) {
+        this.decimalNumber = decimalNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDecimalNumber() {
+        return decimalNumber;
+    }
+}
